cd82d2b69194faa8c94fb7faa6e43e4c6c931cf0
meganedogYuu <tkra.yuuki@gmail.com>
2016-11-22 15:24:20 +0900

    [change]フォルダ・ファイルの整理

---
+++ Assets/Scripts/start.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour {
+	public void OnClick() {
+		// Debug.Log("Start Button click!");
+		// 非表示にする
+		//gameObject.SetActive(false);
+
+		SceneManager.LoadScene ("ModeSelectScene");
+		// Application.LoadLevel("ModeSelectScene");
+	}
+}

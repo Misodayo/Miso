fe340475494e87ef43edec4b796653b379d927e5
meganedogYuu <tkra.yuuki@gmail.com>
2016-11-22 16:48:57 +0900

    [add]アドバイスシーンを追加

---
+++ Assets/Scripts/modeSelect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class modeSelect : MonoBehaviour {
+
+	public void OnClick() {
+
+		switch (transform.name) {
+		case "AdviceBtn":
+			SceneManager.LoadScene ("AdviceScene");
+			break;
+		case "TraningBtn":
+
+			break;
+		default:
+			break;
+		}
+	}
+}

35fd18e59cbc024c30920504648e71d697df6747
meganedogYuu <tkra.yuuki@gmail.com>
2016-11-27 17:43:46 +0900

    [add]ペースメーカー選択画面を作成

---
+++ Assets/Scripts/PacemakerSelect.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PacemakerSelect : MonoBehaviour {
+
+	public void OnClick() {
+
+		SceneManager.LoadScene("PacemakerSelectScene");
+	}
+}
